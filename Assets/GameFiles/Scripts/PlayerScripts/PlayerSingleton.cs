d0b213df6f11ac6dabb7344e4ad44f5734b063e5
unitedgamedevelopers <krushilgamedev20@gmail.com>
2021-09-10 16:55:43 +0530

    Initial Setup

---
+++ Assets/GameFiles/Scripts/PlayerScripts/PlayerSingleton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleton : MonoBehaviour
+{
+    #region Properties
+    public static PlayerSingleton Instance = null;
+    #endregion
+
+    #region MonoBehaviour Functions
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+    #endregion
+}
