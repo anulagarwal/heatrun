f92e8a884417de62837376cb8857dd6df09042eb
unitedgamedevelopers <krushilgamedev20@gmail.com>
2021-09-14 16:16:34 +0530

    VFX Rise and Drop

--- Assets/GameFiles/Scripts/PlayerScripts/PlayerCollisionAndTriggerEventsHandler.cs
+++ Assets/GameFiles/Scripts/PlayerScripts/PlayerCollisionAndTriggerEventsHandler.cs
@@ -8,6 +8,8 @@ public class PlayerCollisionAndTriggerEventsHandler : MonoBehaviour
     [Header("Components Reference")]
     [SerializeField] private PlayerMovementHandler playerMovementHandler = null;
     [SerializeField] private PlayerTemperatureHandler playerTemperatureHandler = null;
+    [SerializeField] private ParticleSystem tempRiseVFX = null;
+    [SerializeField] private ParticleSystem tempDropVFX = null;
     #endregion
 
     #region MonoBehaviour Functions
@@ -16,6 +18,22 @@ public class PlayerCollisionAndTriggerEventsHandler : MonoBehaviour
         if (other.gameObject.tag == "Obstacle")
         {
             playerTemperatureHandler.UpdatePlayerTemperature(other.gameObject.GetComponent<ObstaclesHandler>().GetTemperature);
+            if (other.gameObject.GetComponent<ObstaclesHandler>().GetTemperature < 0)
+            {
+                tempRiseVFX.Stop();
+                if (!tempDropVFX.isPlaying)
+                {
+                    tempDropVFX.Play();
+                }
+            }
+            else if (other.gameObject.GetComponent<ObstaclesHandler>().GetTemperature > 0)
+            {
+                tempDropVFX.Stop();
+                if (!tempRiseVFX.isPlaying)
+                {
+                    tempRiseVFX.Play();
+                }
+            }
             Destroy(other.gameObject);
         }
         else if (other.gameObject.tag == "Finish")
