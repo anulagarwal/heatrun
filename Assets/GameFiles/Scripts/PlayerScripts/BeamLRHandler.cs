86f00c794199a4f62ae7bfdd7110f8445014c490
unitedgamedevelopers <krushilgamedev20@gmail.com>
2021-09-29 14:57:31 +0530

    Beam

---
+++ Assets/GameFiles/Scripts/PlayerScripts/BeamLRHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamLRHandler : MonoBehaviour
+{//
+    #region Properties
+    [Header("Components Reference")]
+    [SerializeField] private LineRenderer lineRenderer = null;
+    [SerializeField] private Transform beamTip = null;
+    #endregion
+
+    #region MonoBehaviour Functions
+    private void Update()
+    {
+        lineRenderer.SetPosition(1, beamTip.localPosition);
+    }
+    #endregion
+}
